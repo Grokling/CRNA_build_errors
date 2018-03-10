a6ab00859f74dd0bf016694994f07bab53ab32b1
Simeon Theobald <simeon.theobald@gmail.com>
2018-03-10 22:46:12 +1300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import redux from 'redux'
+import reduxsaga from 'redux-saga'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Text>If this renders, it's working</Text>
+    );
+  }
+}
